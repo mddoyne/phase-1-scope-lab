133293b5249428973762bf07000506f104e3e57c
mark <mddoyne@asu.edu>
2022-05-05 18:19:19 -0600

    commit tree e553b08253edd7a7f7a495673b48e579443184b6

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName()
+{  
+    var name = customerName;
+    name.toUpperCase();
+    return name;
+}
+
+upperCaseCustomerName();
+
+function setBestCustomer()
+{
+    var bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer()
+{
+
+}
+
+changeLeastFavoriteCustomer()
+{
+    const leastFavoriteCustomer = 0
+}
+
+changeLeastFavoriteCustomer()
+{
+    
+}
